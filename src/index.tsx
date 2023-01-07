16191529dafb6dd61904c1cf259816398ce9c19c
biometricPsychography <cyclinglinguist@gmail.com>
2023-01-06 19:46:30 -0600

    Migrate to TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Landing from './pages/landing/Landing';
 import TableView from './pages/table-view/TableView';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route exact path='/' exact element={<Landing />} />
+        <Route path='/' element={<Landing />} />
         <Route path='/table-view' element={<TableView />} />
       </Routes>
     </Router>
@@ -21,4 +24,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();

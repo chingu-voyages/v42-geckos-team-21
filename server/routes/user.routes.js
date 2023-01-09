eda3880a661e0311523e4284e6e009c62b8d26d1
Sherline Au <au.sherline@gmail.com>
2023-01-09 17:38:06 -0600

    added route for logout

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,5 +4,6 @@ module.exports = app => {
   app.get(`/api/users`, UserController.getAllUsers)
   app.post(`/api/users/new`, UserController.register)
   app.post(`/api/login`, UserController.login)
+  app.get(`/api/logout`, UserController.logout)
   app.get(`/api/users/getloggedinuser`, UserController.getLoggedInUser)
 }

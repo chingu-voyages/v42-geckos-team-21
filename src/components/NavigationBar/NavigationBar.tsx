20f0fce912a2626a7cbb1de78b29b0f176b52c32
William Shaner <williamshaner3@gmail.com>
2023-01-11 13:49:25 -0600

    added responsiveness to navbar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import "./NavigationBar.css"
 import {AiOutlineMenu} from 'react-icons/ai'
+import {CgWorkAlt} from 'react-icons/cg'
 const NavigationBar = () => {
   const [loggedIn, setLoggedIn] = useState(false)
   const [dropdownOpen, setDropdownOpen] = useState(false)
@@ -15,7 +16,7 @@ const NavigationBar = () => {
         <Link onClick={() => setDropdownOpen(false)} to="/table-view" className='nav-logo'>
           <h1>
           <span className='nav-logo-big'>Job</span><br />
-          <span className='nav-logo-small'>Tracker</span>
+          <span className='nav-logo-small'>Tracker <CgWorkAlt /></span>
             </h1></Link>
 
         <ul className='nav-menu'>

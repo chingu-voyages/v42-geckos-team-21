d9b81fd839f311cfac9786d2a3cb8df3175b38ce
biometricPsychography <cyclinglinguist@gmail.com>
2023-01-12 11:44:32 -0600

    fix: correctly link styled checkboxes to labels

--- client/src/pages/table-view/table-components/Row.tsx
+++ client/src/pages/table-view/table-components/Row.tsx
@@ -19,8 +19,8 @@ function Row(props: props) {
                     <RowCellTextInput identifier='date' />
                     <td>
                         <div className='td-flex-wrapper'>
-                            <input id="cover-letter-check-row-2" type="checkbox" name="cover-letter-check-row-2" />
-                            <label className='check' htmlFor="cover-letter-check-row-2">
+                            <input id={`${props.identifier}-cover-letter-check-row`} type="checkbox" name={`${props.identifier}-cover-letter-check-row`} />
+                            <label className='check' htmlFor={`${props.identifier}-cover-letter-check-row`}>
                                 <svg version="1.1" viewBox="0 0 3600 3600" xmlns="http://www.w3.org/2000/svg">
                                     <path d="m1446.5 2125.8c239.36-397.04 895.74-858.15 1232.3-1096.6l96.023 63.574c-432.11 365.55-976.25 810.56-1120.3 1303.3-96.023 31.781-304.08 143.04-336.09 174.83-112.03-238.4-332.34-575.68-493.37-672.54 304.2-357.64 529.3 40.691 621.41 227.52z" />
                                 </svg>
@@ -29,8 +29,8 @@ function Row(props: props) {
                     </td>
                     <td>
                         <div className='td-flex-wrapper'>
-                            <input id="reached-out-check-row-2" type="checkbox" name="reached-out-check-row-2" />
-                            <label className='check' htmlFor="reached-out-check-row-2">
+                            <input id={`${props.identifier}-reached-out-check-row`} type="checkbox" name={`${props.identifier}-reached-out-check-row`} />
+                            <label className='check' htmlFor={`${props.identifier}-reached-out-check-row`}>
                                 <svg version="1.1" viewBox="0 0 3600 3600" xmlns="http://www.w3.org/2000/svg">
                                     <path d="m1446.5 2125.8c239.36-397.04 895.74-858.15 1232.3-1096.6l96.023 63.574c-432.11 365.55-976.25 810.56-1120.3 1303.3-96.023 31.781-304.08 143.04-336.09 174.83-112.03-238.4-332.34-575.68-493.37-672.54 304.2-357.64 529.3 40.691 621.41 227.52z" />
                                 </svg>
@@ -53,8 +53,8 @@ function Row(props: props) {
                     <td>08/09/22</td>
                     <td>
                         <div className='td-flex-wrapper'>
-                            <input id="cover-letter-check-row-2" type="checkbox" name="cover-letter-check-row-2" />
-                            <label className='check' htmlFor="cover-letter-check-row-2">
+                            <input id={`${props.identifier}-cover-letter-check-row`} type="checkbox" name={`${props.identifier}-cover-letter-check-row`} />
+                            <label className='check' htmlFor={`${props.identifier}-cover-letter-check-row`}>
                                 <svg version="1.1" viewBox="0 0 3600 3600" xmlns="http://www.w3.org/2000/svg">
                                     <path d="m1446.5 2125.8c239.36-397.04 895.74-858.15 1232.3-1096.6l96.023 63.574c-432.11 365.55-976.25 810.56-1120.3 1303.3-96.023 31.781-304.08 143.04-336.09 174.83-112.03-238.4-332.34-575.68-493.37-672.54 304.2-357.64 529.3 40.691 621.41 227.52z" />
                                 </svg>
@@ -63,8 +63,8 @@ function Row(props: props) {
                     </td>
                     <td>
                         <div className='td-flex-wrapper'>
-                            <input id="reached-out-check-row-2" type="checkbox" name="reached-out-check-row-2" />
-                            <label className='check' htmlFor="reached-out-check-row-2">
+                            <input id={`${props.identifier}-reached-out-check-row`} type="checkbox" name={`${props.identifier}-reached-out-check-row`} />
+                            <label className='check' htmlFor={`${props.identifier}-reached-out-check-row`}>
                                 <svg version="1.1" viewBox="0 0 3600 3600" xmlns="http://www.w3.org/2000/svg">
                                     <path d="m1446.5 2125.8c239.36-397.04 895.74-858.15 1232.3-1096.6l96.023 63.574c-432.11 365.55-976.25 810.56-1120.3 1303.3-96.023 31.781-304.08 143.04-336.09 174.83-112.03-238.4-332.34-575.68-493.37-672.54 304.2-357.64 529.3 40.691 621.41 227.52z" />
                                 </svg>

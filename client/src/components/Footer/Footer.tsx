7a37b4f656a52f7ae23dcbfd7cd73fe1f1e9464f
William Shaner <williamshaner3@gmail.com>
2023-01-12 09:39:52 -0600

    landing page and footer merging

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import "./Footer.css"
 const Footer = () => {
   return (
-<<<<<<< HEAD:client/src/components/Footer/Footer.tsx
-    <footer><div>Footer</div></footer>
-=======
     <footer>
       <h4>&copy; 2023 JOB TRACKER</h4>
       <hr />
       <p>created by <a rel="noreferrer" href="https://www.seldoncortex.com/" target="_blank">Stan</a>, <a rel="noreferrer" href="https://github.com/sherlineau" target="_blank">Sherline</a>, and <a rel="noreferrer" href="https://genuine-sunflower-520c38.netlify.app/" target="_blank">Will</a></p>
        </footer>
 
->>>>>>> 0f37abbd (added nav dropdown for small screens, added footer):src/components/Footer/Footer.js
   )
 }
 

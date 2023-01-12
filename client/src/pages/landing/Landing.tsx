ee076c2715f7057904684e7c1830e31135957c57
William Shaner <williamshaner3@gmail.com>
2023-01-12 13:46:03 -0600

    created login container with inputs

--- client/src/pages/landing/Landing.tsx
+++ client/src/pages/landing/Landing.tsx
@@ -7,8 +7,8 @@ function Landing() {
       <h1 className='landing-title-one'>Make your job search easy.</h1> 
       
      <img src={LandingImg} alt="landing" className='landing-image'/> <h2 className='landing-title-two'>Keep all your applications in one place.</h2>
-       <Link to="/table-view" className='login-btn'>Login</Link>
-       <p>New here? <Link to="/login" className='sign-up-btn'>Sign up</Link></p>
+       <Link to="/login" className='login-btn'>Login</Link>
+       <p>New here? <Link to="/signup" className='sign-up-btn'>Sign up</Link></p>
     </div>
   );
 }

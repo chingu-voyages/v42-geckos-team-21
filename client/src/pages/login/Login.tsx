b93c3a2d8f779f4ad9e8cbf98f7129b584f168c9
William Shaner <williamshaner3@gmail.com>
2023-01-15 08:05:11 -0600

    merge conflict

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,44 +1,43 @@
-import React, {useRef, useState} from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 import './Login.css'
 const Login = () => {
-  const usernameField = useRef()
-  const passwordField = useRef()
-  const [formErrors, setFormErrors] = useState({})
+  const [inputEmail, setInputEmail] = useState('');
+  const [inputPassword, setInputPassword] = useState('');
 
-  const handleSubmit = (ev) => {
-    ev.preventDefault();
-    console.log('handle form here');
-    const username = usernameField.current.value;
-    const password = passwordField.current.value;
-
-    const errors = {};
-    if(!username){
-      errors.username= 'Username must not be empty.';
-    }
-    if(!password) {
-      errors.password = 'Password must not be empty.';
-    }
-    setFormErrors(errors);
-    if(Object.keys(errors).length > 0) {
-      return
-    }
-  }
+ 
   return (
     <main className='login-container'>
       <form className='login-content' onSubmit={handleSubmit}>
         <h3>Login</h3>
-        <label htmlFor='username'>Username</label>
-        <input ref={usernameField} type='text' name='username' required/>
-        <p className='error'>{formErrors.username}</p>
-        <label htmlFor='username'>Password</label>
-        <input ref={passwordField} type='text' name='password' required/>
-        <p className='error'>{formErrors.password}</p>
+        <input type='text' name='email' onChange={(e) => {
+          setInputEmail(e.target.value);
+        }}/>
+        <label htmlFor='email'>Email</label>
+        <input type='text' name='password' onChange={(e) => {
+          setInputPassword(e.target.value);
+        }}/>
+        <label htmlFor='email'>Password</label>
         <button type='submit' className='login-submit'>Log In</button>
         <p>Not a member? <Link to='/signup'>Sign up now</Link></p>
         </form>
     </main>
   )
+
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    
+    axios.post('http://localhost:3001/api/login', {email: inputEmail, password: inputPassword})
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => console.error(err.message))
+
+  }
 }
+
+
 
 export default Login

0b8a5da1bca6b46fe5c913d2179e603186402f32
William Shaner <williamshaner3@gmail.com>
2023-01-14 22:21:38 -0600

    login page error handling

--- client/src/pages/landing/Landing.tsx
+++ client/src/pages/landing/Landing.tsx
@@ -3,13 +3,13 @@ import LandingImg from "../../../src/assets/landing.png"
 import { Link } from 'react-router-dom';
 function Landing() {
   return (
-    <div className="landing centered-column">
+    <main className="landing centered-column">
       <h1 className='landing-title-one'>Make your job search easy.</h1> 
       
      <img src={LandingImg} alt="landing" className='landing-image'/> <h2 className='landing-title-two'>Keep all your applications in one place.</h2>
        <Link to="/login" className='login-btn'>Login</Link>
        <p>New here? <Link to="/signup" className='sign-up-btn'>Sign up</Link></p>
-    </div>
+    </main>
   );
 }
 
--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import { Link } from 'react-router-dom'
 import './Login.css'
 const Login = () => {
+  const usernameField = useRef()
+  const passwordField = useRef()
+  const [formErrors, setFormErrors] = useState({})
 
-  const handleSubmit = () => {
-    document.querySelector('.login-form').preventDefault()
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    console.log('handle form here');
+    const username = usernameField.current.value;
+    const password = passwordField.current.value;
+
+    const errors = {};
+    if(!username){
+      errors.username= 'Username must not be empty.';
+    }
+    if(!password) {
+      errors.password = 'Password must not be empty.';
+    }
+    setFormErrors(errors);
+    if(Object.keys(errors).length > 0) {
+      return
+    }
   }
   return (
     <main className='login-container'>
-      <form className='login-content' onSubmit={() => handleSubmit()}>
+      <form className='login-content' onSubmit={handleSubmit}>
         <h3>Login</h3>
-        <input type='text' name='username' />
         <label htmlFor='username'>Username</label>
-        <input type='text' name='password' />
+        <input ref={usernameField} type='text' name='username' required/>
+        <p className='error'>{formErrors.username}</p>
         <label htmlFor='username'>Password</label>
+        <input ref={passwordField} type='text' name='password' required/>
+        <p className='error'>{formErrors.password}</p>
         <button type='submit' className='login-submit'>Log In</button>
         <p>Not a member? <Link to='/signup'>Sign up now</Link></p>
         </form>

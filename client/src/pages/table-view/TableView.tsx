0026bd427a33cc5dc12e50de6b6019bb1f9d3d6c
biometricPsychography <cyclinglinguist@gmail.com>
2023-01-27 16:44:53 -0600

    feat: show loading indicator while fetching user

--- client/src/pages/table-view/TableView.tsx
+++ client/src/pages/table-view/TableView.tsx
@@ -8,7 +8,7 @@ import { IfcUser } from '../..';
 import { NullLiteral } from 'typescript';
 
 interface IfcProps {
-  user: IfcUser
+  user: IfcUser | null
 }
 
 export interface IfcApplicationFromDb {
@@ -45,6 +45,53 @@ function TableView(props: IfcProps) {
 
   let [areRowsFromDBParsedState, setAreRowsFromDBParsedState] = useState(false);
 
+
+  if (props.user === null) {
+    return (
+      <div className="TableView">
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">Company</th>
+              <th scope="col">Position</th>
+              <th scope="col">Date Applied</th>
+              <th scope="col">Cover Letter</th>
+              <th scope="col">Reached Out</th>
+              <th scope="col">Notes</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr id="add-job-button-row">
+              <td colSpan={6}><button onClick={() => setJobRowState(
+                oldJobRowState => {
+                  let newNumber = oldJobRowState.slice().sort((a, b) => b.identifier - a.identifier)[0].identifier + 1;
+                  return ([{
+                    identifier: newNumber,
+                    isInitiallyNew: true,
+                    key: newNumber,
+                    user: props.user,
+                    setAlertText,
+                    setAlertKey
+                  }] as IfcCommonJobRowProps[]).concat(oldJobRowState)
+                }
+              )}>
+                Enter new job&nbsp;
+                <svg width="1200pt" height="1200pt" version="1.1" viewBox="0 0 1200 1200" xmlns="http://www.w3.org/2000/svg">
+                  <path d="m951.6 248.4c-194.4-194.4-510-194.4-704.4 0-194.4 194.4-194.4 510 0 704.4s510 194.4 704.4 0c194.4-194.4 194.4-511.2 0-704.4zm-126 380.4c-7.1992 7.1992-18 12-28.801 12h-156v156c0 10.801-4.8008 21.602-12 28.801-7.1992 7.1992-18 12-28.801 12-22.801 0-40.801-18-40.801-40.801v-156h-156c-22.801 0-40.801-18-40.801-40.801s18-40.801 40.801-40.801h156v-156c0-22.801 18-40.801 40.801-40.801s40.801 18 40.801 40.801v156h156c22.801 0 40.801 18 40.801 40.801-1.2031 10.801-4.8008 21.602-12 28.801z" />
+                </svg>
+              </button></td>
+            </tr>
+  
+            <tr><td colSpan={6}>Loading . . .</td></tr>
+  
+          </tbody>
+        </table>
+        <Alert text={alertText} exitAfterDuration={10000} alertKey={alertKey}
+          setAlertKey={setAlertKey} />
+      </div>
+  
+    );
+  }
 
 
 

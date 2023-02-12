48f7b063c4a384bfd414ca728a7d4337c026c978
Stan <cyclinglinguist@gmail.com>
2023-02-12 13:36:26 -0600

    fix: link login button on landing to login page

--- client/src/pages/landing/Landing.tsx
+++ client/src/pages/landing/Landing.tsx
@@ -6,7 +6,7 @@ function Landing() {
     <main className="landing row">
       <h1 className='landing-title-one'>Make your job search easy.</h1> 
      <img src={LandingImg} alt="landing" className='landing-image'/> <h2 className='landing-title-two'>Keep all your applications in one place.</h2>
-       <Link to="/table-view" className='login-btn'>Login</Link>
+       <Link to="/login" className='login-btn'>Login</Link>
        <p>New here? <Link to="/sign-up" className='sign-up-btn'>Sign up</Link></p>
     </main>
   );

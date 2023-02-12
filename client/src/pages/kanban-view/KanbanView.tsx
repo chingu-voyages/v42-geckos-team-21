8c8e747e214c11a7ddbc92e46ed058f717b22828
Stan <cyclinglinguist@gmail.com>
2023-02-12 15:53:45 -0600

    feat: added under construction content for Kanban

--- client/src/pages/kanban-view/KanbanView.tsx
+++ client/src/pages/kanban-view/KanbanView.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./KanbanView.css"
+import underConstruction from "../../assets/undraw_under_construction.svg"
 
 const KanbanView = () => {
   return (
-    <div className='KanbanView'>KanbanView</div>
+    <div className='KanbanView'>
+      <h2>Under construction...</h2>
+      <img  src={underConstruction} alt='Under construction...'/>
+    </div>
   )
 }
 
